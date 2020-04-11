82485def2909b5fae418c5dbef0986b984d8aaf6
anoopmunshi <anoopmunshi@users.noreply.github.com.com>
2020-04-11 20:13:11 +0530

    Embed Map updated based on Lat Long
    Showing Embed Map with Zoom level at z=13.

--- foodcenterdetail.php
+++ foodcenterdetail.php
@@ -24,11 +24,15 @@ Dinner: <?php echo implode($data['timings']['dinner'], '-'); ?> </span>
                                     
                            </div>
                        </div>
-                       
-                      <!-- <div class="p-2 white-bg mt-3">
-                      <iframe src='https://www.google.com/maps?q=37.819722,-122.478611' width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        
+
+                      <div class="p-2 white-bg mt-3">
+
+                      <iframe src='https://maps.google.com/maps?q=<?php echo $data['lat']; ?>, <?php echo $data['long']; ?>&t=&z=13&ie=UTF8&iwloc=&z=16&output=embed' width="100%" height="250" frameborder="0" style="border:0" allowfullscreen>
+                        
+                      </iframe>
                                <button type="button" class="btn  btn-lg btn-block btn-info glyphicon glyphicon-share-alt "> </button>
-                       </div> -->
+                       </div>
            </div>
 
 				
