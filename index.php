2a4de9ead10030ce7e3455173910a5d00c6be02c
anoopmunshi <anoopmunshi@users.noreply.github.com.com>
2020-04-12 15:33:04 +0530

    http to https
    minor change

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $count = json_decode($countData, true);
 
 	<!-- Google places Autocomplete -->
 
-	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=+<?php echo base64_decode($places_key); ?>+&sensor=false&libraries=places"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=+<?php echo base64_decode($places_key); ?>+&sensor=false&libraries=places"></script>
 	<script type="text/javascript">
 		google.maps.event.addDomListener(window, 'load', function () {
 			var places = new google.maps.places.Autocomplete(document.getElementById('autocomplete_search'));
