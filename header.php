a11923f6ff8ca95df0e9561c69e3900d431e357c
anoopmunshi <anoopmunshi@users.noreply.github.com.com>
2020-04-13 21:36:42 +0530

    meta added for social share

--- header.php
+++ header.php
@@ -1,84 +1,99 @@
 <!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<title>Zero Food - A List of free food centers across India </title>
+<html lang="en">
+<head>
+	<title>Zero Food - A List of free food centers across India </title>
 
-			<!-- favicon -->
-			<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
-			<link rel="icon" href="img/favicon.png" type="image/x-icon">
+	<!-- favicon -->
+	<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+	<link rel="icon" href="img/favicon.png" type="image/x-icon">
 
 
-			<!-- meta -->
-			<meta charset="UTF-8">
-		    <meta name="viewport" content="width=device-width, initial-scale=1">
-		    <meta charset="utf-8">
-		    <meta name="description" content="Find list of free food centers during COVID-19 outbreak.">
-		    <meta name="keywords" content="free food, Covid free food, Coronovirus Free Food, Community Kitchens">
-		    <meta name="google-site-verification" content="X0_EhVGmTWu5ecqt4qRBGW1SUZ-ZWPVlwzdlR48CsEo" />
-			<meta property="og:url"                content="http://zerofood.in" />
-			<meta property="og:type"               content="article" />
-			<meta property="og:title"              content="Zero Food - A List of free food centers across India" />
-			<meta property="og:description"        content="Find free food centers across India" />
-			<meta property="og:image"              content="img/eradicate.png" />
+	<!-- meta -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="description" content="Find list of free food centers during COVID-19 outbreak.">
+	<meta name="keywords" content="free food, Covid free food, Coronovirus Free Food, Community Kitchens">
+	<meta name="google-site-verification" content="X0_EhVGmTWu5ecqt4qRBGW1SUZ-ZWPVlwzdlR48CsEo" />
 
 
 
 
-		    <!-- css -->
-			<link rel="stylesheet" href="css/bootstrap.min.css">
-			<link rel="stylesheet" href="css/ionicons.min.css">
-			<link rel="stylesheet" href="css/pace.css">
-		    <link rel="stylesheet" href="css/custom.css">
-		    <link rel="stylesheet" href="css/jquery.dataTables.min.css">
-		    <link rel="stylesheet" href="css/font-awesome.min.css">
-
-		    <style type="text/css">
-		   
-		   	.grecaptcha-badge{display:none;}
-
-		    </style>
-    
-
-		    <!-- js -->
-		    <script src="js/jquery-2.1.3.min.js"></script>
-		    <script src="js/bootstrap.min.js"></script>
-		    <script src="js/pace.min.js"></script>
-		    <script src="js/modernizr.custom.js"></script>
-		    <script src="js/jquery.dataTables.min.js" ></script>
+	<meta property="og:url"                content="http://zerofood.in" />
+	<meta property="og:type"               content="article" />
+	<meta property="og:title"              content="Zero Food - A List of free food centers across India" />
+	<meta property="og:description"        content="Find free food centers across India" />
+	<meta property="og:image"              content="img/eradicate.png" />
 
 
-			<!--  // Google JS reCAPTCHA v2-->
+
+	<meta name="twitter:card" content="This wesbite helps poor people not go hungry" />
+	<meta name="twitter:site" content="@ZeroFoodIn" />
+	<meta name="twitter:creator" content="@ZeroFoodIn" />
+	<meta property="og:url" content="http://zerofood.in" />
+	<meta property="og:title" content="Zero Food - A List of free food centers across India" />
+	<meta property="og:description" content="Find free food centers across India with a simple search option, the quickly get to know who is providing free food" />
+	<meta property="og:image" content="mg/eradicate.png" />
+
+
+
+
+
+	<!-- css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/ionicons.min.css">
+	<link rel="stylesheet" href="css/pace.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<style type="text/css">
+
+		.grecaptcha-badge{display:none;}
+
+	</style>
+
+
+	<!-- js -->
+	<script src="js/jquery-2.1.3.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/pace.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	<script src="js/jquery.dataTables.min.js" ></script>
+
+
+	<!--  // Google JS reCAPTCHA v2-->
 
 <!-- 			<script src="https://www.google.com/recaptcha/api.js?render=6Le4ougUAAAAADCC0zd-jMm012n1MRvhwmz4-NTB" async defer></script>
- -->
+-->
 
-			<!-- Global site tag (gtag.js) - Google Analytics -->
-			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163391752-1"></script>
-			<script>
-			  window.dataLayer = window.dataLayer || [];
-			  function gtag(){dataLayer.push(arguments);}
-			  gtag('js', new Date());
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163391752-1"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-			  gtag('config', 'UA-163391752-1');
-			</script>
-
-
+	gtag('config', 'UA-163391752-1');
+</script>
 
 
 
-			<!-- Hotjar Tracking Code for http://www.zerfood.in -->
-			<script>
-			    (function(h,o,t,j,a,r){
-			        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-			        h._hjSettings={hjid:1764424,hjsv:6};
-			        a=o.getElementsByTagName('head')[0];
-			        r=o.createElement('script');r.async=1;
-			        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-			        a.appendChild(r);
-			    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-			</script>
+
+
+<!-- Hotjar Tracking Code for http://www.zerfood.in -->
+<script>
+	(function(h,o,t,j,a,r){
+		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		h._hjSettings={hjid:1764424,hjsv:6};
+		a=o.getElementsByTagName('head')[0];
+		r=o.createElement('script');r.async=1;
+		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		a.appendChild(r);
+	})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
 
 
-		    
-		</head>
+
+</head>
