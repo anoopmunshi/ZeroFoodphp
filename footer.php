2e9fa8212c01a5ae4a6175587ffa19c15c096c14
anoopmunshi <anoopmunshi@users.noreply.github.com.com>
2020-04-13 18:18:57 +0530

    disclaimer

--- footer.php
+++ footer.php
@@ -4,13 +4,18 @@
 			<div class="row">
 				<div class="col-md-12">
 
-					<p class="copyright">A community effort By Anonymous Citizens</p>
+					<p class="copyright">A community effort By Anonymous Citizens
+					</p>
 					<br>
 					<div class="col-md-4 col-md-offset-4">
 
 						<a href="https://t.me/zerofoodIndia" target="_blank"   class="btn btn-info btn-block" role="button">Join Telegram</a>
 						
+					<br>
+					<p class="text-center"> <a href="disclaimer.html" >Disclaimer</a> </p>
 					</div>
+
+
 
 				</div>
 			</div>
